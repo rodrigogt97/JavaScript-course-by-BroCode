d77f08135e07dc376f8d05db6d09a375f79c97ac
Luis_chavez <lrodrigochavez@hotmail.com>
2024-06-05 20:27:53 -0600

    se agregar mas ejercicios de strings

--- strings/strings.js
+++ strings/strings.js
@@ -12,3 +12,12 @@ let domain = email.slice(email.indexOf('@'));
 console.log(username)
 console.log(domain)
 
+const fullName = 'Luis Rodrigo';
+
+let firstName =fullName.slice(0,fullName.indexOf(' '));
+
+let lastName = fullName.slice(fullName.indexOf(" ") +1);
+
+console.log(firstName);
+
+console.log(lastName);
