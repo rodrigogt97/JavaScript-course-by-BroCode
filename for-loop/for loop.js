12bf525a644e448bef63e5d08f152f9aac083f5e
Luis_chavez <lrodrigochavez@hotmail.com>
2024-06-14 21:08:14 -0600

    se agregan mejores cambios al codigo

---
+++ for-loop/for loop.js
@@ -0,0 +1,21 @@
+// for loop
+
+// repeat some code a limited amount of times
+
+for(let i =0; i<=2; i++){
+  console.log('hello');
+}
+
+for (let i =1; i<+20; i++){
+  
+
+  // we use this to sking an interaction
+  if(i==13){
+    continue;
+  }
+  else{
+    console.log(i);
+  }
+  
+}
+
---
+++ for-loop/for-loop2.js
@@ -0,0 +1,19 @@
+for (let i=2; i <=10; i+=3){
+  console.log(i)
+}
+
+
+// there are up to three statements we can write
+
+//  first one, create a temporary variable
+
+// condition in which we should continue
+
+// increment or decrement our counter
+
+for (let i=2; i <=10; i+=3){
+  console.log(i)
+}
+
+
+
