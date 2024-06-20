3a4bb1b3c7203bb7ad4ff5b02da90afbe2a049b9
Luis_chavez <lrodrigochavez@hotmail.com>
2024-06-20 10:17:32 -0600

    se agregan ejercicios sobre arrays

---
+++ array/index.js
@@ -0,0 +1,14 @@
+// array = a variable like structure that can hold more than 1 value
+
+let fruit = 'apple';
+
+let frutas = ['apple', 'orange', 'banana'];
+
+console.log (frutas[0]);
+
+frutas.push('coconut');
+
+frutas.unshift('mango');
+
+frutas.shift(); // para poner elementos al inicio del array
+
